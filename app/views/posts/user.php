db342cb8c83551540595ef3b0e0f312c94155498
Nonso Mgbechi <mgbechinonso@gmail.com>
2018-10-06 23:42:41 +0100

    Modified

--- app/views/posts/user.php
+++ app/views/posts/user.php
@@ -4,33 +4,14 @@
 		<div class="col-md-6">
 			<h1><?php echo ($data["user"]->id == $_SESSION["user_id"]) ? "Your" : $data["user"]->name . "'s";?> Posts</h1>
 		</div>
-<<<<<<< HEAD
-		
-	</div>
-	<?php $i = 0;?>
-	<?php foreach($data["posts"] as $posts): ?>
-		<div class="card-body mb-3 p-2">
-=======
 	</div>
 	<?php foreach($data["posts"] as $posts): $ppl = $this->postModel->postPreviouslyLiked($posts->postId);?>
 	<div class = "card bg-light mb-3">
 		<div class="card-body mb-3 pl-5 pr-5 p-2">
->>>>>>> likes
 			<h4 class="card-title"><?php echo $posts->title?></h4>
 			<div class="bg-light mb-3">Written By <a href="<?php echo URLROOT;?>/posts/user/<?php echo $data["user"]->name."/".$posts->userId;?>">
 				<?php echo $data["user"]->name; ?></a> On <?php echo $posts->postCreated; ?></div>
 			<p class="card-text"><?php echo $posts->body; ?></p>
-<<<<<<< HEAD
-			<input type="hidden" id="pid<?=$i;?>" value="<?= $posts->id;?>">
-			<button id="like--btn<?=$i;?>" class="btn btn-primary pull-right">
-  				Like <span class="badge badge-light">4</span>
-			</button>
-			<a href="<?php echo URLROOT;?>/posts/show/<?php echo $posts->id;?>" class="btn btn-dark">More</a>
-		</div>
-	<?php $i++; ?>
-	<?php endforeach; ?>
-<?php require_once APPROOT. "/views/inc/footer.php";?>
-=======
 			
 			<div>
 				<a href="<?php echo URLROOT;?>/posts/show/<?php echo $posts->postId;?>" class="btn btn-dark pull-right ml-2">More</a>
@@ -75,4 +56,3 @@
 		</div>
 	</div>
 <?php require_once APPROOT. "/views/inc/footer.php";?>
->>>>>>> likes

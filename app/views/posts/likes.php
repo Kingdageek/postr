4e227cb981ec3501361bab14e525820176e4bcd8
Nonso Mgbechi <mgbechinonso@gmail.com>
2018-10-12 04:21:41 +0100

    Added the divs in the loop so the users will be displayed on different lines

--- app/views/posts/likes.php
+++ app/views/posts/likes.php
@@ -1,11 +1,13 @@
-<div style="border-bottom: solid 1px #ccc; padding: 1rem;">
 <?php if (count($data['likes']) > 0):?>
-<?php foreach ($data['likes'] as $post_likes):?>    
+<?php foreach ($data['likes'] as $post_likes):?>  
+<div style="border-bottom: solid 1px #ccc; padding: 1rem;">
     <a href="<?= URLROOT;?>/posts/user/<?= $post_likes->name."/".$post_likes->userId;?>" style="text-decoration: none;font-size: 1em;">
         <?= $post_likes->name; ?>
     </a>  
+</div>
 <?php endforeach;?>
 <?php else: ?>
+<div style="border-bottom: solid 1px #ccc; padding: 1rem;">
     <p>There are currently no likes for this post.</p>
-<?php endif;?>
 </div>
+<?php endif;?>

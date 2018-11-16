c37ee3f779abd9c9d180fce622f591dbe2e10478
Nonso Mgbechi <mgbechinonso@gmail.com>
2018-11-16 18:07:25 +0100

    Added ids to the body textareas

--- app/views/posts/add.php
+++ app/views/posts/add.php
@@ -15,7 +15,7 @@
           <div class="form-group">
           	<label for="body">Body:<sup> *</sup></label>
           	<textarea name="body" class="form-control form-control-lg 
-          	<?php echo (!empty($data['body_err']) ? 'is_invalid' : ''); ?>">
+          	<?php echo (!empty($data['body_err']) ? 'is_invalid' : ''); ?>" id="body">
               <?php echo $data['body'];?>
             </textarea>
           	<span class="invalid-feedback"><?php echo $data['body_err'];?></span>
